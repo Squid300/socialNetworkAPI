782e3331f89c5088c165fc90c192a2609658bb8b
Squid300 <squid400@gmail.com>
2022-08-30 13:04:29 -0400

    added controllers and routes for user

---
+++ controllers/userController.js
@@ -0,0 +1,71 @@
+const { User, Reaction, Thought } = require('../models');
+
+module.exports = {
+    getUsers( req, res ) {
+        User.find()
+            .then(( users ) => res.json( users ))
+            .catch(( err ) => res.status(500).json( err ));
+    },
+    getSingleUser( req, res ) {
+        User.findOne({ _id: req.params.userId })
+            .then(( user ) => 
+            !user
+                ? res.status(404).json({ message: "No user with that id" })
+                : res.json(user)
+            )
+            .catch(( err ) => res.status(500).json( err ));
+    },
+    createUser( req, res ) {
+        User.Create( req.body )
+            .then(( user ) => res.json( user ))
+            .catch(( err ) => res.json( err ));
+    },
+    updateUser( req, res ) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then(( user ) =>
+                !user
+                    ? res.status(404).json({ message: "no user with that id" })
+                    : res.json(user)
+            )
+            .catch(( err ) => res.status(500).json( err ));
+    },
+    deleteUser( req, res ) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then(( user ) =>
+                !user
+                    ? res.status(404).json({ message: "no user with that id" })
+                    : res.json( user )
+            )
+            .catch(( err ) => res.status(500).json( err ));
+    },
+    addFriend( req, res ) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId }},
+            { runValidators: true, new: true }
+        )
+            .then(( user ) =>
+                !user
+                    ? res.status(404).json({ message: "no user with that id" })
+                    : res.json( user )
+            )
+            .catch(( err ) => res.status(500).json( err ));
+    },
+    deleteFriend( req, res ) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId }},
+            { runValidators: true, new: true }
+        )
+            .then(( user ) =>
+            !user
+                ? res.status(404).json({ message: "no user with that id" })
+                : res.json( user )
+            )
+            .catch(( err ) => res.status(500).json( err ));
+    }
+};
---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+
+router.use( '/users', userRoutes );
+router.use( ( req, res ) => res.send( "Wrong route" ));
+
+module.exports = router;
